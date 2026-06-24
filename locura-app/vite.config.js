bb8216af5ad61c38cc5425331704119de73c4c39
Carlos Osegueda <carlosegueda2002@gmail.com>
2026-06-23 22:56:32 -0600

    trusted origins

--- locura-app/vite.config.js
+++ locura-app/vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
+const allowedHosts = [ 'carlos.authcode.biz'];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  server: {
+    allowedHosts,
+  },
 })
+
