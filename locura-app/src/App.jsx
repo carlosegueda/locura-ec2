27b8a78a5da5f255e1e9f4b97a362d2a543ac924
Carlos Osegueda <carlosegueda2002@gmail.com>
2026-06-23 20:37:08 -0600

    Init

---
+++ locura-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1>ESTA APP ES UNA COMPLETA LOCURA</h1>
+      <p>coming soon</p>
+    </div>
+    </>
+  )
+}
+
+export default App
